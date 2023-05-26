6bd6c0490ad8a1ba443c43766d64ce5c82b45c0d
Oleg Gnatyk <103590840+GnatykOleg@users.noreply.github.com>
2023-05-26 16:46:32 +0300

    feat: Add compononents for make order and install formik
    - Install formik
    - Update CartPage with styles
    - Create FormMakeOrder components with styles adn schemf makeOrderSchema

---
+++ src/components/forms/FormMakeOrder/FormMakeOrder.tsx
@@ -0,0 +1,109 @@
+import { useFormik } from "formik";
+import { TextField } from "@mui/material";
+
+import { makeOrderSchema } from "../schemas/makeOrderSchema";
+
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../../../helpers/hooks/redux-hooks";
+
+import { clearCart } from "../../../redux/cart/cartSlice";
+
+import { makeOrderOperation } from "../../../redux/cart/cartOperations";
+
+import {
+  getCartDataSelector,
+  getCartTotalPriceSelector,
+} from "../../../redux/cart/cartSelectors";
+
+import MyButton from "../../common/MyButton/MyButton";
+import MyTitle from "../../common/MyTitle/MyTitle";
+
+import * as Styled from "./FormMakeOrder.styled";
+
+const FormMakeOrder = () => {
+  const cart = useAppSelector(getCartDataSelector);
+
+  const cartTotalPrice = useAppSelector(getCartTotalPriceSelector);
+
+  const totalPriceText = `Total price: ${cartTotalPrice.toFixed(2)}$`;
+
+  const dispatch = useAppDispatch();
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      phone: "",
+      adress: "",
+    },
+    // Вернуть валидацию
+    validationSchema: makeOrderSchema,
+    onSubmit: (values, { resetForm }) => {
+      const order = { ...values, total_price: cartTotalPrice, orders: cart };
+
+      dispatch(clearCart());
+
+      dispatch(makeOrderOperation(order));
+
+      resetForm();
+    },
+  });
+
+  return (
+    <Styled.MyForm onSubmit={formik.handleSubmit}>
+      <div>
+        <MyTitle text={totalPriceText} as="h2" variant="h4" />
+      </div>
+
+      <TextField
+        id="name"
+        name="name"
+        label="Name"
+        value={formik.values.name}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.name && Boolean(formik.errors.name)}
+        helperText={formik.touched.name && formik.errors.name}
+      />
+
+      <TextField
+        id="email"
+        name="email"
+        label="Email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.email && Boolean(formik.errors.email)}
+        helperText={formik.touched.email && formik.errors.email}
+      />
+
+      <TextField
+        id="phone"
+        name="phone"
+        label="Phone"
+        value={formik.values.phone}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.phone && Boolean(formik.errors.phone)}
+        helperText={formik.touched.phone && formik.errors.phone}
+      />
+
+      <TextField
+        id="adress"
+        name="adress"
+        label="Adress"
+        value={formik.values.adress}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        error={formik.touched.adress && Boolean(formik.errors.adress)}
+        helperText={formik.touched.adress && formik.errors.adress}
+      />
+
+      <MyButton text="Make Order" type="submit" />
+    </Styled.MyForm>
+  );
+};
+
+export default FormMakeOrder;
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,16 +1,83 @@
-import { Container, Typography } from "@mui/material";
-import { visuallyHidden } from "@mui/utils";
+import { useState } from "react";
 
-import { MySection } from "../../components";
+import { useAppSelector } from "../../helpers/hooks/redux-hooks";
 
-const CartPage = () => (
-  <MySection>
-    <Typography variant="h2" component={"h2"} sx={visuallyHidden}>
-      Cart page
-    </Typography>
+import { getCartDataSelector } from "../../redux/cart/cartSelectors";
 
-    <Container fixed></Container>
-  </MySection>
-);
+import {
+  ProductsList,
+  MyTitle,
+  MySection,
+  FormMakeOrder,
+  MyButton,
+  MobileSidebar,
+  FullSidebar,
+} from "../../components";
+
+import { useMediaQuery } from "@mui/material";
+
+import { BREAKPOINTS } from "../../helpers/constants/theme-constants";
+
+import * as Styled from "./CartPage.styled";
+
+const { MIN_DESKTOP, MAX_TABLET } = BREAKPOINTS;
+
+const CartPage = () => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState<boolean>(false);
+
+  const cart = useAppSelector(getCartDataSelector);
+
+  const sidebarHandler = () => setIsSideBarOpen(state => !state);
+
+  const maxTabletWidth = useMediaQuery(`(max-width:${MAX_TABLET})`);
+
+  const minDesktopWidth = useMediaQuery(`(min-width:${MIN_DESKTOP})`);
+
+  return (
+    <Styled.MyPageWrapper>
+      {cart.length > 0 && maxTabletWidth && (
+        <>
+          <MySection>
+            <MyTitle text="Hero section" as="h2" hidden />
+            <MyButton
+              onClick={sidebarHandler}
+              text="Show dialog to make order"
+              styles={{ margin: "0 auto" }}
+            />
+          </MySection>
+
+          <MobileSidebar
+            isSideBarOpen={isSideBarOpen}
+            sidebarHandler={sidebarHandler}
+          >
+            <FormMakeOrder />
+          </MobileSidebar>
+        </>
+      )}
+
+      {cart.length > 0 && minDesktopWidth && (
+        <MySection>
+          <MyTitle text="Left sidebar make order form" as="h2" hidden />
+          <FullSidebar>
+            <FormMakeOrder />
+          </FullSidebar>
+        </MySection>
+      )}
+
+      {cart.length > 0 && (
+        <MySection styles={{ width: "100%" }}>
+          <MyTitle text="Cart product list" as="h2" hidden />
+          <ProductsList products={cart} />
+        </MySection>
+      )}
+
+      {cart.length === 0 && (
+        <MySection styles={{ width: "100%" }}>
+          <MyTitle text="Your cart is currently empty😔" as="h2" variant="h2" />
+        </MySection>
+      )}
+    </Styled.MyPageWrapper>
+  );
+};
 
 export default CartPage;
